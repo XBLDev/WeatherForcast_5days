140fe9f5dcf016768f3da14722d5d4630ee031d3
XBLDev <u5260839@anu.edu.au>
2018-01-09 13:52:22 +1100

    better UI Layout

--- WeatherForcast.js
+++ WeatherForcast.js
@@ -1,51 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const passport = require('passport');
-// const config = require('./config');
-// var path = require('path');
-
-// connect to the database and load models
-// require('./server/models').connect(config.dbUri);
-
-
 const app = express();
-// tell the app to look for static files in these directories
+const path = require('path');
+
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
-
-// tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
-// pass the passport middleware
-// app.use(passport.initialize());
-
-// load passport strategies
-// const localSignupStrategy = require('./server/passport/local-signup');
-// const localLoginStrategy = require('./server/passport/local-login');
-// passport.use('local-signup', localSignupStrategy);
-// passport.use('local-login', localLoginStrategy);
-
-// pass the authenticaion checker middleware
-// const authCheckMiddleware = require('./server/middleware/auth-check');
-// app.use('/api', authCheckMiddleware);
-
-// routes
-// const authRoutes = require('./server/routes/auth');
-// const apiRoutes = require('./server/routes/api');
-// const newsRoutes = require('./server/routes/news');
-// const eventRoutes = require('./server/routes/Events');
-// const galaryPhotoRoutes = require('./server/routes/GalaryPhotos');
-
-// app.use('/auth', authRoutes);
-// app.use('/api', apiRoutes);
-// app.use('/news', newsRoutes);
-// app.use('/Events', eventRoutes);
-// app.use('/GalaryPhotos', galaryPhotoRoutes);
 
 app.route('/*').get(function(req, res) { 
   return res.sendFile(path.join(__dirname, './server/static/index.html')); 
 });
 
-// start the server
 app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080 or http://127.0.0.1:8080');
 });
--- client/src/components/start.jsx
+++ client/src/components/start.jsx
@@ -1,232 +1,175 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      UsrLat : '',
-      UsrLon : '',
-      GettingData: false,
-      WeatherDetails: [],
-      WeatherSummaries: []
-    };
-  }
-
-  componentWillMount()
-  {
-    /**
-     * Get user's Lat and Lon for the openweathermap api call, 
-     * The openweathermap api doens't seem to have a function to 
-     * detect user's location
-     */
-    const xhr = new XMLHttpRequest(); 
-    xhr.open("GET", "http://ip-api.com/json", true);
-    xhr.addEventListener('load', () => {
-      if (xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          this.setState({
-            UsrLat: response.lat,
-            UsrLon: response.lon,   
-            GettingData: true            
-          });                      
-      }
-    });
-    xhr.send();  
-  }
-
-  componentDidUpdate(prevProps, prevState)
-  {
-
-    if(this.state.GettingData === true)
-    {
-      var RequestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=".concat(this.state.UsrLat).concat("&lon=")
-      .concat(this.state.UsrLon).concat("&mode=json&APPID=secretKeyHere");
-      const xhr = new XMLHttpRequest(); 
-      xhr.open("GET", RequestUrl, true);
-      xhr.addEventListener('load', () => {
-        if (xhr.status === 200) {
-
-            var Response = JSON.parse(xhr.responseText);
-            var Summaries = [];
-            var Details = [];
-            var Dates = [];
-            var AveTemp = [];
-            var CurrentDayRecordNum = 0;
-
-            for(var i=0 ; i < Response.list.length; i++)
-            {              
-                var dt_txt = Response.list[i].dt_txt;
-                var date = dt_txt.substring(0, dt_txt.lastIndexOf(' '));
-                
-                if(Dates.indexOf(date) === -1)
-                /**
-                 * If we don't have any detail for a date, create new detail list for that date, 
-                 * and add summaries for the previous date if at least one date's weather summary
-                 * is generate, in this case the summary is just the average temprature for a date.
-                 */                
-                {
-                  Dates.push(date);
-                  if(AveTemp.length > 0)
-                  {
-                    AveTemp[AveTemp.length-1] /= CurrentDayRecordNum;
-                    Summaries.push([]);
-                    Summaries[Summaries.length-1].push(
-                     <div key={dt_txt}>
-                           <h2>{Dates[Dates.length-2]}</h2>
-                           <p>Average Temperature: {AveTemp[AveTemp.length-1]}</p>
-                     </div>                    
-                    );
-                  }
-                  CurrentDayRecordNum = 1;
-                  
-                  AveTemp.push(Number(Response.list[i].main.temp));
-                  Details.push([]);
-                  Details[Details.length-1].push(
-                  <div key={dt_txt}>
-                  <h2>
-                    {"Time" + ": " + dt_txt.substring(dt_txt.lastIndexOf(" ")+1, dt_txt.length)}
-                  </h2>
-                  </div>);
-                  for (var key in Response.list[i].main) {
-                  Details[Details.length-1].push
-                  (<div key={dt_txt.concat(key + ": " + Response.list[i].main[key])}>
-                    {key + ": " + Response.list[i].main[key]}
-                    </div>);
-                  }
-                }
-                else
-                {
-                /**
-                 * If we have already details for a date, add records to the details for that date,
-                 * and if the last record is reached, calculate the average temperature for the last
-                 * date, and add a new summary for the last date
-                 */        
-                  AveTemp[Dates.indexOf(date)] += Number(Response.list[i].main.temp);
-                  CurrentDayRecordNum += 1;
-                  
-                  Details[Dates.indexOf(date)].push(
-                  <div key={dt_txt}>
-                    <h2>
-                      {"Time" + ": " + dt_txt.substring(dt_txt.lastIndexOf(" ")+1, dt_txt.length)}
-                    </h2>
-                  </div>);
-
-                  for (var key in Response.list[i].main) {
-                    Details[Dates.indexOf(date)].push(
-                    <div key={dt_txt.concat(key + ": " + Response.list[i].main[key])}>
-                      {key + ": " + Response.list[i].main[key]}
-                    </div>
-                    );
-                  }
-                  
-                  if(i === Response.list.length - 1)
-                  {
-                    AveTemp[AveTemp.length-1] /= CurrentDayRecordNum;
-                    Summaries.push([]);
-                    Summaries[Summaries.length-1].push(
-                     <div key={dt_txt}>
-                           <h2>{Dates[Dates.length-1]}</h2>
-                           <p>Average Temperature: {AveTemp[AveTemp.length-1]}</p>
-                     </div>                    
-                    );
-                  }
-                }
-            }          
-            
-            this.setState({
-              WeatherDetails: Details,
-              WeatherSummaries: Summaries,
-              GettingData : false
-            });
-            
-        }
-      });
-      xhr.send();  
+    constructor(props) {
+        super(props);
+        this.state = {
+            UsrLat: '',
+            UsrLon: '',
+            UsrCity: '',
+            CurrentDisplayingDay: 0,
+            GettingData: false,
+            DataLoaded: false,            
+            WeatherDetails: []
+        };
     }
-  }
 
-  render() {
+    onDayClicked(value) {
+        this.setState({ CurrentDisplayingDay: value });
+    }
+
+    componentWillMount() {
+        /**
+         * Get user's Lat and Lon for the openweathermap api call, 
+         * The openweathermap api doens't seem to have a function to 
+         * detect user's location
+         */
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://ip-api.com/json", true);
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                var Response = JSON.parse(xhr.responseText);
+                this.setState({
+                    UsrLat: Response.lat,
+                    UsrLon: Response.lon,
+                    GettingData: true,
+                    DataLoaded: false
+                });
+            }
+        });
+        xhr.send();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (this.state.GettingData === true) {
+            var RequestUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=".concat(this.state.UsrLat).concat("&lon=")
+                .concat(this.state.UsrLon).concat("&units=metric&cnt=5&mode=json&APPID=secretkey");
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", RequestUrl, true);
+            xhr.addEventListener('load', () => {
+                if (xhr.status === 200) {
+                    var Response = JSON.parse(xhr.responseText);
+                    var Details = [];
+                    var WeatherCellElements = [];
+                    for (var i = 0; i < Response.list.length; i++) {
+                        Details.push({});
+                        Details[Details.length - 1].Date = moment().add(i, 'days').format('DD/MM/YY');
+                        Details[Details.length - 1].Clouds = Response.list[i].clouds;
+                        Details[Details.length - 1].Humidity = Response.list[i].humidity;
+                        Details[Details.length - 1].WindSpeed = Response.list[i].speed;
+                        Details[Details.length - 1].Temp = Response.list[i].temp.day;
+                        Details[Details.length - 1].TempMax = Response.list[i].temp.max;
+                        Details[Details.length - 1].TempMin = Response.list[i].temp.min;
+                        Details[Details.length - 1].WeatherDescription = Response.list[i].weather[0].description;
+                        Details[Details.length - 1].WeatherIcon = "http://openweathermap.org/img/w/".concat(Response.list[i].weather[0].icon).concat(".png");
+                    }
+                }
+
+                this.setState({
+                    UsrCity: Response.city.name,
+                    WeatherDetails: Details,
+                    GettingData: false,
+                    DataLoaded: true
+                });
+            });
+            xhr.send();
+        }
+    }
+
+    render() {
+
+        var DayCells = [];
+        for (var i = 0; i < this.state.WeatherDetails.length; i++) {
+            DayCells.push(
+                <div key={i} className="daycell" onClick={this.onDayClicked.bind(this, i)}>
+                    <div className={this.state.CurrentDisplayingDay == i ? "bg-primary" : "bg-info"}>
+                        {this.state.WeatherDetails[i].Date.substring(0, this.state.WeatherDetails[i].Date.lastIndexOf('/'))}<br />
+                        <img src={this.state.WeatherDetails[i].WeatherIcon} /><br />
+                        {this.state.WeatherDetails[i].TempMax.toFixed(0)}&#176; {this.state.WeatherDetails[i].TempMin.toFixed(0)}&#176;
+                    </div>
+                </div>
+            );
+        }
 
         return (
-          <div>
-              <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                  <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">Weather Forcast: 5 days</a>
-                  </div>
-                  <div id="navbar" className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                      <li className="active"><a href="#">Home</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
+            <div>
+                <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">WeatherForcast</a>
+                        </div>
+                        <div id="navbar" className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Home</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container" id="centerarea">
+                    <div className="row" >
+                        <div className="col col-lg-6 col-md-10 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-1" id="firstrow">
+                            <div className="weather">
+                                <div className="current">
+                                    {this.state.DataLoaded ? (
+                                        <div className="info">
+                                            <div className="bg-primary">
+                                                <div>&nbsp;</div>
+                                                <div className="city">{String(this.state.UsrCity).toUpperCase()}</div>
+                                                <div className="citytime">
+                                                    {String(this.state.WeatherDetails[this.state.CurrentDisplayingDay].Date).toUpperCase()}
+                                                </div>
+                                                <div className="weatherdescription">
+                                                    {String(this.state.WeatherDetails[this.state.CurrentDisplayingDay].WeatherDescription).toUpperCase()}
+                                                </div>
+                                                <div className="temp">
+                                                    <img src={this.state.WeatherDetails[this.state.CurrentDisplayingDay].WeatherIcon} className="tempicon" />
+                                                    {this.state.WeatherDetails[this.state.CurrentDisplayingDay].Temp.toFixed(1)}
+                                                    <small><small><small><small><small><small><small><small>&#8451;</small></small></small></small></small></small></small></small>                                                    
+                                                </div>
+                                                <div>&nbsp;</div>
+                                            </div>
+                                        </div>) :
+                                        (
+                                            <div>LOADING...</div>
+                                        )
+                                    }
 
-              <div className="container">
-                  <div className="row content" id="centerarea">
-                      <div className="col-md-8 text-left" > 
-                            <div className="container" id="firstweatherarea">
-                              <div className="page-header">
-                                <h1>{this.state.WeatherSummaries[0]}</h1>
-                                {this.state.WeatherDetails[0]}
-                              </div>     
+                                    {this.state.DataLoaded ? (
+                                        <div className="icon">
+                                            <div>&nbsp;</div>
+                                            <div>HUMIDITY: {String(this.state.WeatherDetails[this.state.CurrentDisplayingDay].Humidity).toUpperCase()}%</div>
+                                            <div>WIND: {String(this.state.WeatherDetails[this.state.CurrentDisplayingDay].WindSpeed).toUpperCase()} m/s</div>
+                                            <div>CLOUDINESS: {String(this.state.WeatherDetails[this.state.CurrentDisplayingDay].Clouds).toUpperCase()}%</div>
+                                            <div>&nbsp;</div>
+                                        </div>) :
+                                        (
+                                            <div></div>
+                                        )
+                                    }
+
+                                </div>
                             </div>
-                      </div>
-                  
-                      <div className="col-md-4 sidenav" id="rightsidemenu">
-                          <div className="panel-group" id="accordion">
-                              <div className="panel panel-default">
-                                  <div className="panel-heading">
-                                      <h4 className="panel-title">
-                                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">{this.state.WeatherSummaries[1]}</a>
-                                      </h4>
-                                  </div>
-                                  <div id="collapseOne" className="panel-collapse collapse">
-                                      <div className="panel-body">{this.state.WeatherDetails[1]}</div>
-                                  </div>
-                              </div>
-                              <div className="panel panel-default">
-                                  <div className="panel-heading">
-                                      <h4 className="panel-title">
-                                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">{this.state.WeatherSummaries[2]}</a>
-                                      </h4>
-                                  </div>
-                                  <div id="collapseTwo" className="panel-collapse collapse">
-                                      <div className="panel-body">{this.state.WeatherDetails[2]}</div>
-                                  </div>
-                              </div>
-                              <div className="panel panel-default">
-                                  <div className="panel-heading">
-                                      <h4 className="panel-title">
-                                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">{this.state.WeatherSummaries[3]}</a>
-                                      </h4>
-                                  </div>
-                                  <div id="collapseThree" className="panel-collapse collapse">
-                                      <div className="panel-body">{this.state.WeatherDetails[3]}</div>
-                                  </div>
-                              </div>
-                              <div className="panel panel-default">
-                                  <div className="panel-heading">
-                                      <h4 className="panel-title">
-                                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">{this.state.WeatherSummaries[4]}</a>
-                                      </h4>
-                                  </div>
-                                  <div id="collapseFour" className="panel-collapse collapse">
-                                      <div className="panel-body">{this.state.WeatherDetails[4]}</div>                                     
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>  
+                        </div>
+                        <div
+                            className="col col-lg-6 col-md-10 col-sm-12 col-xs-12 col-lg-offset-3 col-md-offset-1" id="secondrow">
+                            <div className="weatheralldays">
+                                <div className="alldays">
+                                    {DayCells}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
-  }
-} 
+    }
+}
 
 export default App
